26b14c9f24f047d53bc9854555f00ed668457c17
Greg Jordan <greg@ebi-001.ebi.ac.uk>
2009-12-15 23:47:58 +0000

    Do some work on slrsim analysis.

--- projects/slrsim/collect_slrsim.R
+++ projects/slrsim/collect_slrsim.R
@@ -1,44 +1,56 @@
 library(RMySQL)
 drv <- dbDriver('MySQL')
-con <- dbConnect(drv, host='mysql-greg.ebi.ac.uk', port=4134, user='greg', password='TMOqp3now', dbname='slrsim_2xmammals')
+con <- dbConnect(drv, host='mysql-greg.ebi.ac.uk', port=4134, user='slrsim', password='slrsim', dbname='gj1_slrsim_1')
 
 get_vector = function(con,query) {
   res = dbSendQuery(con,query)
-  data = as.vector(fetch(res))
+  data = fetch(res)[[1]]
   dbClearResult(res)
   return(data)
 }
 
 # Get the sim_sets.
-res = dbSendQuery(con,paste(
+sim_sets = get_vector(con,paste(
   'SELECT distinct value FROM protein_tree_tag where tag="sim_set"'
 ))
-sim_sets = as.vector(fetch(res))
-dbClearResult(res)
 
 # Get the parameter sets.
 res = dbSendQuery(con,'SELECT parameter_set_id AS id,parameter_value AS name FROM parameter_set where parameter_name="name";')
 param_sets = as.vector(fetch(res))
 dbClearResult(res)
 
+# Executes a Perl script which extracts the true and inferred omegas for a reference sequence.
 get_data = function(node_id,param_set)  {
   cmd = sprintf("perl ./slrsim_stats_for_node.pl %s %s",node_id,param_set)
-  tbl = read.table(pipe(cmd),header=T,sep="\t")
-  return(tbl)
+  tryCatch(
+    {
+      tbl = read.table(pipe(cmd),header=T,sep="\t")
+      return(tbl)
+    },
+    error=function(err){return()}
+  )
 }
 
-for (sim_set in sim_sets) {
-  sim_results = 
-    
-  node_ids = get_vector(con,sprintf('SELECT distinct node_id FROM protein_tree_tag WHERE tag="sim_set" AND value="%s";',sim_set))
+all_data = data.frame();
+for (parameter_set_id in c(2)) {
+  for (sim_set in sim_sets[3:10]) {
+    node_ids = get_vector(con,sprintf('SELECT distinct node_id FROM protein_tree_tag WHERE tag="sim_set" AND value="%s";',sim_set))
+    for (node_id in node_ids) {
+    print(sprintf("%s %s %s",parameter_set_id,sim_set,node_id))
+      # Grab the site-wise information into a data frame.
+      data = get_data(node_id,parameter_set_id)
 
-  for (node_id in node_ids) {
-    data = get_data(node_id,2)
-    
-  } 
-  
+      if (!is.data.frame(data))
+        next
+
+      # Add some useful info to the data frame.
+      data$sim_set = rep(sim_set,n=nrow(data))
+      data$parameter_set = rep(parameter_set_id,n=nrow(data))
+
+      # Concatenate to the end of the data frame.
+      all_data = rbind(all_data,data)
+    } 
+  }
 }
 
 dbDisconnect(con)
-
-
--- projects/slrsim/slrsim_stats_for_node.pl
+++ projects/slrsim/slrsim_stats_for_node.pl
@@ -4,8 +4,9 @@ use warnings;
 use strict;
 use DBI;
 use Getopt::Long;
-use Bio::EnsEMBL::Registry;
 use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Compara::ComparaUtils;
+use Bio::EnsEMBL::Compara::AlignUtils;
 use Bio::Greg::ComparaLite::HiveUtils;
 use Bio::Greg::EslrUtils;
 use Bio::EnsEMBL::Hive::DBSQL::DBAdaptor;
@@ -17,14 +18,11 @@ my ($url,$clean) = undef;
 GetOptions('url=s' => \$url,
 	   'clean' => \$clean
 	   );
-$url = 'mysql://greg:TMOqp3now@mysql-greg.ebi.ac.uk:4134/gj1_slrsim_1' if (!$url);
-
-my ($mysql_args,$database,$project_base) = undef;
-$mysql_args = Bio::Greg::ComparaLite::HiveUtils->hive_url_to_mysql_args($url);
-$database = Bio::Greg::ComparaLite::HiveUtils->hive_url_to_hashref($url)->{'database'};
-$project_base = getcwd();
+$url = 'mysql://slrsim:slrsim@mysql-greg.ebi.ac.uk:4134/gj1_slrsim_1' if (!$url);
+my $project_base = getcwd();
 
 # Load the adaptors.
+Bio::EnsEMBL::Registry->no_version_check(1);
 my $dba = Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new(-url => $url);
 my $dbc = $dba->dbc;
 my $dbh = $dbc->db_handle;
@@ -35,44 +33,77 @@ get_data_for_node($ARGV[0],$ARGV[1]);
 sub get_data_for_node {
   my $node_id = shift;
   my $parameter_set_id = shift;
-  
+
   my $sa_true;
   my $sa_aln;
+  my @true_entropies;
+  my @aln_entropies;
   my $tree;
   eval {
     $pta->protein_tree_member("protein_tree_member");
     $tree = $pta->fetch_node_by_node_id($node_id);
     $sa_true = $tree->get_SimpleAlign();
+    my $cdna_aln = $tree->get_SimpleAlign(-cdna => 1);
+    @true_entropies = Bio::EnsEMBL::Compara::AlignUtils->column_entropies($cdna_aln);
     
     $pta->protein_tree_member("aln_mcoffee");
     $tree = $pta->fetch_node_by_node_id($node_id);
     $sa_aln = $tree->get_SimpleAlign();
+    $cdna_aln = $tree->get_SimpleAlign(-cdna => 1);
+    @aln_entropies = Bio::EnsEMBL::Compara::AlignUtils->column_entropies($cdna_aln);
+
+    Bio::EnsEMBL::Compara::AlignUtils->indelign($cdna_aln,$tree,{});
+
+    exit(0);
   };
   return if (!$sa_true || !$sa_aln);
 
+  
+
+  my $sim_params = Bio::EnsEMBL::Compara::ComparaUtils->load_params_from_tag($tree,"sim_params");
+  my $param_set_params = Bio::EnsEMBL::Compara::ComparaUtils->load_params_from_param_set($tree->adaptor,$parameter_set_id);
+
+  # Get the sequence to act as a reference in site-wise value comparisons.
+  my $reference_id = '';
+  if (defined $sim_params->{'reference_id'}) {
+    $reference_id = $sim_params->{'reference_id'};
+  }
+
   my @seqs = $sa_true->each_seq;
-  my $seq= $seqs[0];
-  my $name = $seq->id;
-  my $str = $seq->seq;
+  my ($ref_seq) = grep {$_->id eq $reference_id} @seqs;
+  die ("Reference was defined in params but not found in aln!") if ($reference_id ne '' && !defined $ref_seq);
+  $ref_seq = $seqs[0] if (!defined $ref_seq);
+  my $ref_name = $ref_seq->id;
+
+  my $str = $ref_seq->seq;
   my $nogaps = $str;
   $nogaps =~ s/-//g;
 
+  my $aln_table_name = $param_set_params->{'output_table'};
   my $sth1 = $dbh->prepare("SELECT aln_position,omega FROM sitewise_aln WHERE node_id=?;");
-  my $sth2 = $dbh->prepare("SELECT aln_position,omega,type,note FROM omega_mc WHERE node_id=? AND parameter_set_id=?;");
+  my $sth2 = $dbh->prepare("SELECT aln_position,omega,type,note,ncod FROM $aln_table_name WHERE node_id=? AND parameter_set_id=?;");
   $sth1->execute($node_id);
   $sth2->execute($node_id,$parameter_set_id);
   
   my $true_omegas = $sth1->fetchall_hashref('aln_position');
   my $aln_omegas = $sth2->fetchall_hashref('aln_position');
 
-  print "true\taln\n";
+  print join("\t",qw(ref_seq true aln aln_type aln_note ncod true_e aln_e))."\n";
   for (my $i=1; $i <= length($nogaps); $i++) {
-    my $true_col = $sa_true->column_from_residue_number($name,$i);
-    my $aln_col = $sa_aln->column_from_residue_number($name,$i);
+    my $true_col = $sa_true->column_from_residue_number($ref_name,$i);
+    my $aln_col = $sa_aln->column_from_residue_number($ref_name,$i);
+
+#    print "${true_col} ${aln_col}\n";
 
     my $true = $true_omegas->{$true_col}->{'omega'};
     my $aln = $aln_omegas->{$aln_col}->{'omega'};
+    next unless ($aln && $true);
+    my $aln_type = $aln_omegas->{$aln_col}->{'type'};
+    my $aln_note = $aln_omegas->{$aln_col}->{'note'};
+    my $ncod = $aln_omegas->{$aln_col}->{'ncod'};
+    my $true_e = sprintf "%.3f", $true_entropies[$true];
+    my $aln_e = sprintf "%.3f", $aln_entropies[$aln];
 
-    print "$true\t$aln\n";
+    print join("\t",($ref_name,$true,$aln,$aln_type,$aln_note,$ncod,$true_e,$aln_e))."\n";
   }
 }
