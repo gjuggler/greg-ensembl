96f83ba92eac8af2134671cb630259b3585de957
Greg Jordan <gj1@sanger.ac.uk>
2010-10-29 02:10:06 +0100

    Add Evan enrichment lists.

--- projects/2xmammals/evan_enrichments.R
+++ projects/2xmammals/evan_enrichments.R
@@ -2,7 +2,7 @@ load("evan_enrichments.Rdata")
 source("go_enrichments.R")
 
 genes$stable_id = genes$human_protein
-for (list in c('list1','list2','list3','list4','list5','list6')) {
+for (list in c('list1','list2','list3','list4','list5','list6','list7','list8','list9')) {
   print(list)
 
   data = read.csv(paste("evan_",list,".txt",sep=""),header=F)
@@ -15,12 +15,12 @@ for (list in c('list1','list2','list3','list4','list5','list6')) {
 
   print(paste("before",nrow(data),"after",nrow(merged)))
 
-#  tbl = get.enrich.by.subset(
-#    subset=merged$stable_id,
-#    all=genes$stable_id,
-#    go.df=go.hs.iea,
-#    go.field.name='stable_id'
-#  )
-#  head(tbl)
-#  write.csv(tbl,file=paste("evan_enrich_",list,".csv",sep=""),row.names=F)
+  tbl = get.enrich.by.subset(
+    subset=merged$stable_id,
+    all=genes$stable_id,
+    go.df=go.hs.iea,
+    go.field.name='stable_id'
+  )
+  head(tbl)
+  write.csv(tbl,file=paste("evan_enrich_",list,".csv",sep=""),row.names=F)
 }
