855621facde7eeceaee8a99b25b5ee622a588570
gjuggler <greg@ebi.ac.uk>
2011-01-14 13:30:32 +0000

    2xmammals webpage output work.

---
+++ projects/2xmammals/webpage_bed_format.as
@@ -0,0 +1,15 @@
+table hg18KGchr7
+"UCSC Genes for chr7 with color plus GeneSymbol and SwissProtID"
+(
+string  chrom;          "Reference sequence chromosome or scaffold"
+uint    txStart;        "Transcription start position"
+uint    txEnd;          "Transcription end position"
+string  name;           "Name of gene"
+uint    score;          "Score"
+char[1] strand;         "+ or - for strand"
+uint    cdsStart;       "Coding region start"
+uint    cdsEnd;         "Coding region end"
+string  color;          "Color based on Score"
+string  geneSymbol;     "Ensembl Protein ID"
+float value;            "Float-encoded score value"
+)
--- projects/2xmammals/webpage_output.R
+++ projects/2xmammals/webpage_output.R
@@ -48,8 +48,7 @@ write.rows.page <- function(page_base,rows,css.txt) {
   file.name <- paste(page_base,'.html',sep='')
   file.name <- gsub('[- \\(\\)/\\\\]','_',file.name)
 
-  print(paste("Writing page",file.name
-))
+  print(paste("Writing page",file.name))
   page <- openPage(file.name,
     title=paste(page_base),
     link.javascript=c('sorttable.js','tablefilter.js'),
@@ -61,6 +60,95 @@ write.rows.page <- function(page_base,rows,css.txt) {
   return(file.name)
 }
 
+bed.output <- function(rows,color.field,color.lo,color.hi) {
+  # BED wants '+' or '-' strand.
+  strand <- rows$chr_strand
+  strand[strand == 1] <- '+'
+  strand[strand == -1] <- '-'
+  rows$chr_strand <- strand
+
+  rows <- subset(rows,!is.na(Chr))
+  rows[rows$Chr == 'chrMT',]$Chr <- 'chrM'
+
+  color.values <- rows[,color.field]
+  if (color.field == 'Pos-sel score') {
+    color.values[color.values > 30] <- 30
+  } else if (color.field == 'Overall dN/dS') {
+    color.values[color.values > 1] <- 1
+  } else if (color.field == 'Tree length') {
+    color.values[color.values > 0.7] <- 0.7
+  }
+  color.values.no.na <- color.values[!is.na(color.values)] # Remove NAs.
+  # Scale color values from 0 to 1.
+  color.values[is.na(color.values)] <- 0
+  color.range <- (max(color.values,na.rm=T)-min(color.values,na.rm=T))
+  color.values <- (color.values - min(color.values,na.rm=T)) / color.range
+  #print(summary(color.values))
+  color.ramp <- colorRamp(c(color.lo,color.hi))
+  colors <- color.ramp(color.values)
+  colors.string <- apply(colors,1,function(x) {
+    x <- as.integer(x)
+    z <- paste(x[1],x[2],x[3],sep=',')
+    return(z)
+  })
+  
+  rows$color.tmp <- colors.string
+  rows$score.tmp <- 0
+  bed <- rows[,c('Chr','chr_start','chr_end','Gene','score.tmp','chr_strand','chr_start','chr_end','color.tmp','Ensembl ID',color.field)]
+
+  bed[is.na(bed[,color.field]),color.field] <- 0
+  
+  # Generate the bigBed file.
+  file.name <- paste(color.field,'.bb',sep='')
+  file.name <- gsub('[- \\(\\)/\\\\]','_',file.name)
+  full.file <- paste(folder,"/",file.name,sep='')
+  if (!file.exists("chrom.sizes")) {
+    system("fetchChromSizes hg19 > chrom.sizes")
+  }
+  print(paste("Outputting BED",file.name))
+  write.table(bed,file="genes_tmp.bed",sep="\t",quote=F,row.names=F,col.names=F)  
+  system("sort -k1,1 -k2,2n genes_tmp.bed > genes_tmp_sorted.bed")
+  system(paste("bedToBigBed -unc -as=webpage_bed_format.as -bedFields=9 genes_tmp_sorted.bed chrom.sizes",full.file))
+
+#  unlink("genes_tmp.bed")
+#  unlink("genes_tmp_sorted.bed")
+
+  rows$color.tmp <- NULL
+  rows$score.tmp <- NULL
+  return(file.name)
+}
+
+bigwig.output <- function(filename) {
+  if (!exists("sites")) { 
+    load("sites_1.Rdata")
+    sites[,'signed_lrt'] = sites$lrt_stat
+    sites[sites$omega < 1,]$signed_lrt = -sites[sites$omega < 1,]$signed_lrt
+    sites <- subset(sites,!is.na(chr_name))
+    sites[sites$chr_name == 'chrMT',]$chr_name <- 'chrM'
+    assign("sites",sites,envir=.GlobalEnv)
+  }
+
+  sites$chr_end <- sites$chr_start+1
+  unique.sites <- sites[!duplicated(sites[,c('chr_name','chr_start')]),]
+
+  bedGraph <- unique.sites[,c('chr_name','chr_start','chr_end','signed_lrt')]
+  bedGraph$chr_start <- sprintf("%d",bedGraph$chr_start)
+  bedGraph$chr_end <- sprintf("%d",bedGraph$chr_end)
+  write.table(bedGraph,file="sites.bedGraph",sep="\t",quote=F,row.names=F,col.names=F)  
+
+  ### Generate the sites bigWig file.
+  sites.filename <- "sitewise_dnds.bw"
+  sites.full.file <- paste(folder,"/",sites.filename,sep='')
+  print(paste("Outputting BigWig",sites.filename))
+  system("sort -k1,1 -k2,2n sites.bedGraph > sites_sorted.bedGraph")
+  system(paste("bedGraphToBigWig -unc sites_sorted.bedGraph chrom.sizes",sites.full.file))
+
+#  unlink("sites.bedGraph")
+#  unlink("sites_sorted.bedGraph")
+
+  return(sites.filename)
+}
+
 # Load data from the table.
 dbname <- 'gj1_2x_57'
 source('~/src/greg-ensembl/scripts/collect_sitewise.R')
@@ -69,13 +157,12 @@ rows <- get.vector(con,query)
 
 
 ### Data clean-up
+###
 # Take neg-log of fisher p-values (easier sorting in web interface)
 rows$pos_sel_score <- as.numeric(sprintf("%.3f",-log(rows$pval_fisher)))
 rows$pval_fisher <- NULL
-
 # Clean up gene names (remove dash at end)
 rows$gene_name <- sub("-.*$","",rows$gene_name)
-
 # Shorten some numbers.
 for (field in c('gc_content_mean','slr_dnds','slr_kappa','seq_length_mean')) {
   rows[,field] <- as.numeric(sprintf("%.2f",rows[,field]))
@@ -83,7 +170,6 @@ for (field in c('gc_content_mean','slr_dnds','slr_kappa','seq_length_mean')) {
 for (field in c('f_neg','f_neut','f_pos')) {
   rows[,field] <- as.numeric(sprintf("%.4f",rows[,field]))
 }
-
 # Rename some columns.
 rename.col <- function(rows,old,new) {
   rows[,new] <- rows[,old]
@@ -118,8 +204,47 @@ new.names <- list(
 for (names in new.names) {
   rows <- rename.col(rows,names[1],names[2])
 }
-
+###
 ### /end Data clean-up
+
+
+### BigBed / BigWig output
+###
+gene.links <- c()
+color.fields <- c('Overall dN/dS','GC','Pos-sel score','Tree length')
+color.lo <- c('black','black','black','black')
+color.hi <- c('blue','red','green','orange')
+for (i in 1:length(color.fields)) {
+  cur.field <- color.fields[i]
+  file.name <- bed.output(rows,cur.field,color.lo[i],color.hi[i])
+
+  # Produce a link to the UCSC browser loading this track.
+  hgct.custom.text <- paste('track',
+                            'type=bigBed',
+                            paste('name=',cur.field,sep=''),
+                            'visibility=squish',
+                            'itemRgb=on',
+                            paste('bigDataUrl=','http://www.ebi.ac.uk/~greg/mammals/',file.name,sep=''),
+                            sep=' ')
+  hgct.custom.encoded <- URLencode(hgct.custom.text)
+  hgct.url <- sprintf("http://genome.ucsc.edu/cgi-bin/hgTracks?db=hg18&position=chr22&hgct_customText=%s",hgct.custom.encoded)
+
+  gene.links <- c(gene.links,hmakeTag('a', cur.field, href=hgct.url)  )
+}
+
+bigwig.file <- bigwig.output(rows)
+hgct.custom.text <- paste('track',
+                          'type=bigWig',
+                          'name=Sitewise dnds',
+                          'visibility=full',
+                          paste('bigDataUrl=','http://www.ebi.ac.uk/~greg/mammals/',bigwig.file,sep=''),
+                          sep=' ')
+hgct.custom.encoded <- URLencode(hgct.custom.text)
+hgct.url <- sprintf("http://genome.ucsc.edu/cgi-bin/hgTracks?db=hg18&position=chr22&hgct_customText=%s",hgct.custom.encoded)
+sitewise.links <- hmakeTag('a','Sitewise dN/dS', href=hgct.url)
+###
+### /end UCSC track output
+
 
 # Constants.
 folder <- '~/scratch/mammals/output/2010-04-30/2010-04-30_01/web'
@@ -151,15 +276,20 @@ table {
 }
 "
 
-
 ### Pages by chromosome.
-chrs <- sort(unique(rows[,'Chr']))
+chrs <- unique(rows[,'Chr'])
+chrs <- chrs[!is.na(chrs)]
+chrs <- sub('chr','',chrs)
+numeric.chrs <- sort(as.numeric(chrs[!is.na(as.numeric(chrs))]))
+chrs <- c(numeric.chrs,chrs[is.na(as.numeric(chrs))])
+chrs <- paste('chr',chrs,sep='')
 for (chr in c('chr22')) {
-#for (chr in chrs[1:2]) {
+#for (chr in chrs) {
   if (is.na(chr)){next}
   sub.rows <- subset(rows, Chr==chr)
   write.rows.page(chr,sub.rows,css.txt)
 }
+print(chrs)
 chr.links <- hmakeTag('a', chrs, href=paste('./',chrs,'.html',sep=''))
 
 ### Top / bottom 100 by field.
@@ -183,21 +313,24 @@ for (field in c('Fraction neutral')) {
   f <- write.rows.page(paste(field,'bottom'),sub.rows,css.txt)
   bot.files <- c(bot.files,f)
 }
-
 top.links <- hmakeTag('a', numeric.fields, href=paste('./',top.files,sep=''))
 bot.links <- hmakeTag('a', numeric.fields, href=paste('./',bot.files,sep=''))
-
-print(bot.links)
 
 ###
 ### HTML template output.
 ###
-html.template <- file("~/src/greg-ensembl/project/2xmammals/webpage_template.html")
+html.template <- file("~/src/greg-ensembl/projects/2xmammals/webpage_template.html")
 html.lines <- readLines(html.template)
 close(html.template)
 html.string <- paste(html.lines,collapse='\n',sep='')
 
+html.string <- sub('CHROMOSOME_LINKS',paste(chr.links,collapse=' '),html.string)
+html.string <- sub('TOP_LINKS',paste(top.links,collapse=' '),html.string)
+html.string <- sub('BOTTOM_LINKS',paste(bot.links,collapse=' '),html.string)
+html.string <- sub('SITEWISE_LINKS',paste(sitewise.links,collapse=' '),html.string)
+html.string <- sub('GENE_LINKS',paste(gene.links,collapse=' '),html.string)
 
+writeChar(html.string,paste(folder,"/webpage_output.html",sep=''))
 
 # Back to original working dir.
 setwd(orig.wd)
